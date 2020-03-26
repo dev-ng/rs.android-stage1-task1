6f348e0f8a9e446c4ab4bdf338904f094312501b
dev-ng <nickolay.golomysov@gmail.com>
2020-03-26 21:16:53 +0100

    Task3 done

--- src/main/kotlin/subtask3/BillCounter.kt
+++ src/main/kotlin/subtask3/BillCounter.kt
@@ -2,9 +2,13 @@ package subtask3
 
 class BillCounter {
 
-    // TODO: Complete the following function
     // The output could be "Bon Appetit" or the string with number(e.g "10")
     fun calculateFairlySplit(bill: IntArray, k: Int, b: Int): String {
-        throw NotImplementedError("Not implemented")
+        val annaToPay = (bill.sum() - bill[k]) / 2
+        val brianOwes = b - annaToPay
+        if(0 == brianOwes){
+            return "Bon Appetit"
+        }
+        return brianOwes.toString()
     }
 }
