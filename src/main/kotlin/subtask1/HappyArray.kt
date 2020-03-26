9b8bd688a38f2f31aae0f1194c19174a70a08ec3
dev-ng <nickolay.golomysov@gmail.com>
2020-03-26 11:02:24 +0100

    Task1 done

--- src/main/kotlin/subtask1/HappyArray.kt
+++ src/main/kotlin/subtask1/HappyArray.kt
@@ -2,8 +2,29 @@ package subtask1
 
 class HappyArray {
 
-    // TODO: Complete the following function
     fun convertToHappy(sadArray: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
+        val size = sadArray.size
+        if(size < 3){
+            return sadArray
+        }
+
+        val happyIndices = ArrayList<Int>()
+        happyIndices.add(0)
+
+        for(i in 1..size - 2) {
+            if(sadArray[i-1] + sadArray[i+1] >= sadArray[i]) {
+                happyIndices.add(i)
+            }
+        }
+
+        happyIndices.add(size-1)
+
+        val result = sadArray.sliceArray(happyIndices)
+
+        if(happyIndices.size == size) {
+            return result
+        }
+
+        return convertToHappy(result)
     }
 }
